a77193c8c1d93d8b7d73d99d8a2b9e3efebdb9bd
Sumit Kumar <sumit6200584775@gmail.com>
2024-09-24 00:03:00 +0530

    update main.jsx

--- frontend/dsa-tracker/src/main.jsx
+++ frontend/dsa-tracker/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {RecoilRoot} from "recoil";
 import AuthObserver from "./recoil/AuthObserver.jsx";
 import {Toaster} from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <RecoilRoot>
-          <AuthObserver />
-          <App />
-          <Toaster />
-      </RecoilRoot>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RecoilRoot>
+            <AuthObserver />
+            <App />
+            <Toaster />
+        </RecoilRoot>
+    </React.StrictMode>,
 )
